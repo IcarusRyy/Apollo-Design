f4928682dd09406c89881176c728a0e7ad3c687c
IcarusRyy <1550588466@qq.com>
2022-05-10 19:06:10 +0800

    Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,32 @@
-import React from "react"
-import "./App.css"
-import Button, { ButtonSize, ButtonType } from "./components/Button/button"
+import React from "react";
+import Button, { ButtonSize, ButtonType } from "./components/Button/button";
 
 function App() {
   return (
     <div className="App">
-      ts-with-react
-      <h1>hello world h1</h1>
-      <h2>hello world h2</h2>
+      <h2>My Button Component</h2>
       <Button>测试 默认</Button>
       <Button btnType={ButtonType.Primary} size={ButtonSize.large}>
-        测试 LargePrimary
+        LargePrimary
       </Button>
       <Button disabled>测试 默认</Button>
       <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
-        测试 Small Danger
+        Small Danger
       </Button>
       <Button
         btnType={ButtonType.Link}
-        size={ButtonSize.large}
-        href="www.baidu.com"
+        size={ButtonSize.Small}
+        href="http://www.baidu.com"
+        target="_blank"
       >
         百度 Large Link
       </Button>
+      <Button btnType={ButtonType.Link} size={ButtonSize.Small} disabled>
+        disable link
+      </Button>
+      <Button btnType={ButtonType.Dashed}>dashed Button</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,5 +1,5 @@
-import classNames from "classnames"
-import React, { FC } from "react"
+import classNames from "classnames";
+import React, { FC } from "react";
 export enum ButtonSize {
   large = "lg",
   Small = "sm",
@@ -9,42 +9,51 @@ export enum ButtonType {
   Default = "default",
   Danger = "danger",
   Link = "link",
+  Dashed = "dashed",
 }
 interface BaseButtonProps {
-  className?: string
-  disabled?: boolean
-  size?: ButtonSize
-  btnType?: ButtonType
-  children: React.ReactNode
-  href?: string
+  className?: string;
+  disabled?: boolean;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  href?: string;
 }
 
-const Button: FC<BaseButtonProps> = (props) => {
-  const { btnType, disabled, size, children, href } = props
+// 获取所有Button的属性 以及 BaseButtonProps     &是ts的联合类型
+type NativeButtonProps = BaseButtonProps &
+  React.ButtonHTMLAttributes<HTMLElement>;
+
+type AnchorButtonProps = BaseButtonProps &
+  React.AnchorHTMLAttributes<HTMLElement>;
+// 将所有的属性设置为可选的
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
+const Button: FC<ButtonProps> = (props) => {
+  const { btnType, disabled, size, children, href, className, ...rest } = props;
   // btn btn-lg , btn-primary
-  const classes = classNames("btn", {
+  const classes = classNames("btn", className, {
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
     disabled: btnType === ButtonType.Link && disabled,
-  })
+  });
   if (btnType === ButtonType.Link) {
     return (
-      <a className={classes} href={href}>
+      <a className={classes} href={href} {...rest}>
         {children}
       </a>
-    )
+    );
   } else {
     return (
-      <button className={classes} disabled={disabled}>
+      <button className={classes} disabled={disabled} {...rest}>
         {children}
       </button>
-    )
+    );
   }
-}
+};
 
 Button.defaultProps = {
   disabled: false,
   btnType: ButtonType.Default,
-}
+};
 
-export default Button
+export default Button;
